3530579765cbe15a20c840d1b7861cc88ad15117
Adam Yang <66138604+adamy03@users.noreply.github.com>
2024-01-18 16:55:23 -0500

    create source folder w/ data + utils

---
+++ source/data.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+import os
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from utils import *
+
+DATA_MASK_DIR = "masks"
+DATA_IMAGE_DIR = "images"
+
+
+class ModisDataset(Dataset):
+    in_channel = 1
+    out_channel = 1
+
+    def __init__(
+        self,
+        data_path,  # save directory
+        image_path=None,  # ground_truth hdf path
+        mask_path=None,  # cloud_covered hdf path
+        transform=None,
+        target_transform=None,
+        image_size=1200,
+        from_existing=False,
+    ):
+        self.data_path = os.path.join(data_path)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.image_size = image_size
+        self.collection = pd.DataFrame(
+            columns=["sample", "ground_truth"]
+        )  # Maps images to original
+
+        # For pre-processed datasets
+        if from_existing:
+            self.collection = pd.read_csv(os.path.join(data_path, "file_mappings.csv"))
+        else:
+            self.image_path = os.path.join(image_path)
+            self.mask_path = os.path.join(mask_path)
+
+            # Create directories for root, root/images, root/mask
+            if not os.path.exists(self.data_path):
+                os.makedirs(self.data_path)
+            if not os.path.exists(os.path.join(self.data_path, DATA_MASK_DIR)):
+                os.makedirs(os.path.join(self.data_path, DATA_MASK_DIR))
+            if not os.path.exists(os.path.join(self.data_path, DATA_IMAGE_DIR)):
+                os.makedirs(os.path.join(self.data_path, DATA_IMAGE_DIR))
+
+            # Construct masked images
+            for tile in os.listdir(image_path):
+                tile_lst = get_lst_day(os.path.join(self.image_path, tile))
+                tile_filename = tile[0:-4] + ".npy"
+
+                np.save(
+                    os.path.join(self.data_path, DATA_IMAGE_DIR, tile_filename),
+                    tile_lst,
+                )
+
+                # Retrieve masks from .hdf files
+                for mask in os.listdir(mask_path):
+                    cloud_mask = get_cloud_mask(os.path.join(self.mask_path, mask))
+
+                    # Checks for error flags
+                    assert list(np.unique(cloud_mask)) == [0, 1], "Mask has data error"
+
+                    # Apply mask over image and save as image
+                    masked = np.multiply(cloud_mask, tile_lst)
+                    masked_filename = (
+                        parse_modis_label(mask)["production_date"] + "." + tile_filename
+                    )
+
+                    np.save(
+                        os.path.join(self.data_path, DATA_MASK_DIR, masked_filename),
+                        masked,
+                    )
+
+                    self.collection.loc[len(self.collection.index)] = [
+                        masked_filename,
+                        tile_filename,
+                    ]
+
+            # Save file name mapping
+            self.collection.to_csv(os.path.join(self.data_path, "file_mappings.csv"))
+
+    def __len__(self):
+        return len(self.collection)
+
+    def __getitem__(self, index):
+        mask = np.load(
+            os.path.join(
+                self.data_path, DATA_MASK_DIR, self.collection.iloc[index]["sample"]
+            )
+        )
+        image = np.load(
+            os.path.join(
+                self.data_path,
+                DATA_IMAGE_DIR,
+                self.collection.iloc[index]["ground_truth"],
+            )
+        )
+
+        if self.transform:
+            mask = self.transform(mask)
+
+        if self.target_transform:
+            image = self.target_transform(mask)
+
+        mask = torch.from_numpy(mask.astype(np.float32))
+        image = torch.from_numpy(image.astype(np.float32))
+
+        return mask, image
---
+++ source/utils.py
@@ -0,0 +1,210 @@
+import earthaccess
+import pandas as pd
+import geopandas as gpd
+from pyhdf.SD import SD, SDC
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+import json
+
+
+def _read_flag(value: int) -> int:
+    """Reads flags of MODIS data 
+
+    Args:
+        value (int): value of MODIS pixel
+
+    Returns:
+        _type_: data flag
+    """
+    bit_string = bin(value)
+    if bit_string[-2::] == "00" or bit_string[-2::] == "01":  # lst produced
+        return 0
+    elif bit_string[-2::] == "10":  # lst not produced, cloud covered
+        return 1
+    elif bit_string[-2::] == "11":  # lst not produced, other
+        return -1
+    else:
+        return value
+
+
+def get_cloud_mask(hdf_path: str) -> np.array:
+    """Creates cloud mask from data flags. Maps over array using _read_flag
+
+    Args:
+        hdf_path (str): path to hdf file
+
+    Returns:
+        np.array: cloud mask: 0-lst, 1-cloud, -1-error
+    """
+    ds = SD(hdf_path, SDC.READ)
+    data = np.array(ds.select("QC_Day").get())
+    vectorized = np.vectorize(_read_flag)
+    qc_day_filtered = vectorized(data)
+    return qc_day_filtered
+
+
+def get_lst_day(hdf_path: str) -> np.array:
+    """Produces array of lst (kelvin) of hdf file
+
+    Args:
+        hdf_path (str): path to hdf file
+
+    Returns:
+        np.array: array of LST in kelvin
+    """
+    ds = SD(hdf_path, SDC.READ)
+    data = np.array(ds.select("LST_Day_1km").get()) * 0.02  # in kelvin
+    return data
+
+
+def get_position(hdf_path: str) -> dict:
+    """Provides position of hdf file
+
+    Args:
+        hdf_path (str): path to hdf file
+
+    Returns:
+        dict: 
+    """
+    ds = SD(hdf_path, SDC.READ)
+    group = "GROUP=GRINGPOINT"
+    end_group = "END_GROUP=GRINGPOINT"
+    lon_str = "OBJECT=GRINGPOINTLONGITUDE"
+    lat_str = "OBJECT=GRINGPOINTLATITUDE"
+    seq_str = "OBJECT=GRINGPOINTSEQUENCENO"
+
+    meta = ds.attributes().get("CoreMetadata.0").split("\n")
+    meta = [str.strip(x).replace(" ", "") for x in meta]
+
+    pos = meta[meta.index(group) : meta.index(end_group)]
+    pos_dict = {}
+
+    for obj in [lon_str, lat_str, seq_str]:
+        str_list = meta[meta.index(obj) + 3].strip("VALUE=")[1:-1].split(",")
+        pos_dict[obj[7::]] = tuple(map(float, str_list))
+
+    return pos_dict
+
+
+def parse_modis_label(path):
+    labels = path.split(".")
+    return {
+        "product_name": labels[0],
+        "acquisition_date": labels[1],
+        "acquisition_time": labels[2],
+        "collection_version": labels[3],
+        "production_date": labels[4],
+    }
+
+
+def query_modis(
+    date_range: tuple,
+    bounding_box: tuple,
+    cloud_cover: tuple,
+    count: int,
+    path: str,
+    create_image=False,
+    create_mask=False,
+):
+    """Queries MODIS data using earth access
+
+    Args:
+        date_range (tuple): date range of query
+        bounding_box (tuple): bounding coordinates to search
+        cloud_cover (tuple): range of percent cloud coverage
+        count (int): number to retrieve from query
+        path (str): path to save data
+        create_image (bool, optional): indicate whether to create jpg images. Defaults to False.
+        create_mask (bool, optional): indicate whether to produce masks. Defaults to False.
+    """
+    # DOI Link: https://doi.org/10.5067/MODIS/MOD11A1.061
+    # also see: https://search.earthdata.nasa.gov/search/granules?p=C1748058432-LPCLOUD&pg[0][v]=f&pg[0][gsk]=-start_date&ff=Available%20in%20Earthdata%20Cloud&tl=1699239359!3!!&fst0=Land%20Surface&lat=-83.40416670000002&long=-118.82965395
+    # data format: https://lpdaac.usgs.gov/documents/715/MOD11_User_Guide_V61.pdf
+    # bounding box params: lower left lon, lower left lat, upper right lon, upper right lat
+
+    earthaccess.login()
+
+    # Retrieve data
+    datasets = earthaccess.search_data(
+        doi="10.5067/MODIS/MOD11A1.061",
+        cloud_hosted=True,
+        temporal=date_range,
+        bounding_box=bounding_box,
+        cloud_cover=cloud_cover,
+        count=count,
+    )
+
+    # Declare Paths
+    path = os.path.join(path)
+    data_path = os.path.join(path, "data")
+    image_path = os.path.join(path, "images")
+    mask_path = os.path.join(path, "masks")
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+        os.makedirs(data_path)
+
+    # Download files
+    earthaccess.download(datasets, data_path)
+
+    # Create and save images
+    if create_image:
+        os.makedirs(image_path)
+
+        for image in os.listdir(data_path):
+            sample = os.path.join(data_path, image)
+            sample_lst = get_lst_day(sample)
+            plt.imsave(
+                os.path.join(
+                    image_path, image.replace(image.split(".")[-1], "") + "jpg"
+                ),
+                sample_lst,
+                cmap="gray",
+            )
+
+    # Create masks
+    if create_mask:
+        os.makedirs(mask_path)
+
+        for image in os.listdir(data_path):
+            sample = os.path.join(data_path, image)
+            sample_mask = get_cloud_mask(sample)
+            plt.imsave(
+                os.path.join(
+                    mask_path, image.replace(image.split(".")[-1], "") + "jpg"
+                ),
+                sample_mask,
+                cmap="gray",
+            )
+
+    # Save query
+    query = {
+        "date": date_range,
+        "bounding_box": bounding_box,
+        "cloud_cover": cloud_cover,
+        "count": count,
+    }
+
+    with open(os.path.join(path, "query.json"), "w") as query_file:
+        json.dump(query, query_file)
+
+
+def retile(img: np.array, tile_size: tuple, image_size: tuple) -> list:
+    """Creates subtiles of images. Assumes dimension of original is divisible by height/width
+
+    Args:
+        img (np.array): original image
+        tile_size (tuple): desired size of tiles
+        image_size (tuple): original image size
+
+    Returns:
+        list: list containing np.arrays of tiles
+    """
+    h, w = tile_size(0), tile_size(1)
+    return [
+        img[x : x + h, y : y + w]
+        for x in range(0, img.shape[0], h)
+        for y in range(0, img.shape[1], w)
+    ]
